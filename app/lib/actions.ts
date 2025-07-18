fb99063ec570f7340a5b53e1a5b86d919241aa07
Kurotrad <dropkuro178gmail.com>
2025-07-18 21:29:59 +0200

    twentytwoth commit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,11 @@
 'use server';
 
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from 'next-auth/react';
+import { AuthError } from 'next-auth';
 
 // Define the form schema using Zod for validation
 const FormSchema = z.object({
@@ -17,17 +20,8 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-import { sql } from '@vercel/postgres';
-
 export async function createInvoice(formData: FormData) {
   const { sellerId, amount, status } = CreateInvoice.parse({
-    sellerId: formData.get('sellerId'),
-    amount: formData.get('amount'),
-    status: formData.get('status'),
-  });
-}
-  export async function updateInvoice(id: string, formData: FormData) {
-  const { sellerId, amount, status } = UpdateInvoice.parse({
     sellerId: formData.get('sellerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
@@ -35,6 +29,24 @@ export async function createInvoice(formData: FormData) {
 
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (seller_id, amount, status, date)
+    VALUES (${sellerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { sellerId, amount, status } = UpdateInvoice.parse({
+    sellerId: formData.get('sellerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
 
   await sql`
     UPDATE invoices
@@ -50,4 +62,22 @@ export async function deleteInvoice(id: string) {
   await sql`DELETE FROM invoices WHERE id = ${id}`;
   revalidatePath('/dashboard/invoices');
 }
-    
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials',);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
